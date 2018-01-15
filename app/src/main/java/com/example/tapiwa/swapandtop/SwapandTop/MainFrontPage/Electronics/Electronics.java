5b295ba68daea0f51c6470f061c14fd4e37b2f50
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-01-15 13:09:24 -0600

    working on electronics gridview

---
+++ app/src/main/java/com/example/tapiwa/swapandtop/SwapandTop/MainFrontPage/Electronics/Electronics.java
@@ -0,0 +1,66 @@
+package com.example.tapiwa.swapandtop.SwapandTop.MainFrontPage.Electronics;
+
+/**
+ * Created by tapiwa on 1/15/18.
+ */
+
+public class Electronics {
+
+
+    private String type, product, description, price, targetSwapPrice, img1, img2,img3,
+                   location;
+
+
+    public Electronics() {
+    }
+
+
+    public Electronics(String type, String product, String price, String targetSwapPrice, String img1,
+                       String img2, String img3, String location) {
+        this.type = type;
+        this.product = product;
+        this.price = price;
+        this.targetSwapPrice = targetSwapPrice;
+        this. img1 = img1;
+        this.img2 = img2;
+        this.img3 = img3;
+        this.location = location;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getTargetSwapPrice() {
+        return targetSwapPrice;
+    }
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
--- app/src/main/java/com/example/tapiwa/swapandtop/SwapandTop/MainFrontPage/Electronics/ElectronicsFrontPageActivity.java
+++ app/src/main/java/com/example/tapiwa/swapandtop/SwapandTop/MainFrontPage/Electronics/ElectronicsFrontPageActivity.java
@@ -3,16 +3,36 @@ package com.example.tapiwa.swapandtop.SwapandTop.MainFrontPage.Electronics;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
 
 import com.example.tapiwa.swapandtop.R;
 import com.example.tapiwa.swapandtop.SwapandTop.MainFrontPage.FrontPage.FrontPageActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class ElectronicsFrontPageActivity extends AppCompatActivity {
+
+    public FirebaseDatabase firebaseDatabase;
+    public FirebaseStorage firebaseStorage;
+    public DatabaseReference electronicsDbRef;
+    public static GridView electronicsGridView;
+
+    public static String ELECTRONICS_DB_REF;
+    public static String ELECTRONICS_ST0RAGE_REF;
+
+    public static Button postNewItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electronics__front__page);
+
+
+
+
     }
 
 }
