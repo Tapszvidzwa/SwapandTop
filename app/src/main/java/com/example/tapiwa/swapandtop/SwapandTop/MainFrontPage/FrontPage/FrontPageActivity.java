50679ec865176b8191fb221b8182b87e95a895c0
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-01-16 12:25:37 -0600

    Included arrows to show categories

--- app/src/main/java/com/example/tapiwa/swapandtop/SwapandTop/MainFrontPage/FrontPage/FrontPageActivity.java
+++ app/src/main/java/com/example/tapiwa/swapandtop/SwapandTop/MainFrontPage/FrontPage/FrontPageActivity.java
@@ -6,16 +6,20 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.tapiwa.swapandtop.R;
 import com.example.tapiwa.swapandtop.SwapandTop.MainFrontPage.Electronics.ElectronicsFrontPageActivity;
 
+import static java.lang.Thread.sleep;
+
 public class FrontPageActivity extends AppCompatActivity {
 
 
-    private TextView furnitureTab, vehiclesTab, electronicsTab, jewelleryTab;
+    private TextView furnitureTab, vehiclesTab, electronicsTab, jewelleryTab, selectCategory;
     private Activity activity;
+    public static ImageView arrow1, arrow2, arrow3, arrow4;
 
 
     @Override
@@ -27,6 +31,13 @@ public class FrontPageActivity extends AppCompatActivity {
         vehiclesTab = findViewById(R.id.vehicles);
         electronicsTab = findViewById(R.id.electronics);
         jewelleryTab = findViewById(R.id.jewellery);
+        selectCategory = findViewById(R.id.select_category_txtV);
+
+        arrow1 = findViewById(R.id.arrow1_imgv);
+        arrow2 = findViewById(R.id.arrow2_imgv);
+        arrow3 = findViewById(R.id.arrow3_imgv);
+        arrow4 = findViewById(R.id.arrow4_imgv);
+
         activity = this;
 
         furnitureTab.setTextColor(Color.argb(138, 0, 0, 0));
@@ -85,6 +96,50 @@ public class FrontPageActivity extends AppCompatActivity {
                 Intent openElectronics = new Intent(FrontPageActivity.this, ElectronicsFrontPageActivity.class);
                 startActivity(openElectronics);
 
+            }
+        });
+
+        selectCategory.setOnClickListener(new View.OnClickListener() {
+
+            //indicate which categories to choose from
+            @Override
+            public void onClick(View view) {
+
+                arrow1.setVisibility(View.VISIBLE);
+                arrow2.setVisibility(View.VISIBLE);
+                arrow3.setVisibility(View.VISIBLE);
+                arrow4.setVisibility(View.VISIBLE);
+
+                Thread thread = new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                            synchronized (this) {
+
+                                wait(200);
+
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+
+                                        arrow1.setVisibility(View.INVISIBLE);
+                                        arrow2.setVisibility(View.INVISIBLE);
+                                        arrow3.setVisibility(View.INVISIBLE);
+                                        arrow4.setVisibility(View.INVISIBLE);
+
+                                    }
+                                });
+
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    };
+                };
+
+                thread.start();
             }
         });
 
